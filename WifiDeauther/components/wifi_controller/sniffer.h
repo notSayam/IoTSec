3ed830c21bdd295ad4c97ad937cf578c970304d7
Sayam Dhingra <43665274+notSayam@users.noreply.github.com>
2023-05-16 23:23:02 -0400

    Add files via upload

---
+++ WifiDeauther/components/wifi_controller/sniffer.h
@@ -0,0 +1,23 @@
+
+#ifndef SNIFFER_H
+#define SNIFFER_H
+
+#include <stdbool.h>
+#include "esp_event.h"
+
+ESP_EVENT_DECLARE_BASE(SNIFFER_EVENTS);
+
+enum {
+    SNIFFER_EVENT_CAPTURED_DATA,
+    SNIFFER_EVENT_CAPTURED_MGMT,
+    SNIFFER_EVENT_CAPTURED_CTRL
+};
+
+
+void wifictl_sniffer_filter_frame_types(bool data, bool mgmt, bool ctrl);
+
+void wifictl_sniffer_start(uint8_t channel);
+
+void wifictl_sniffer_stop();
+
+#endif
